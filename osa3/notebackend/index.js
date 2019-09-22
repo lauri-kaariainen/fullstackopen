9a99f0782067d129c2a1820a297e3c3ac8527df2
Lauri Kääriäinen <lauri.v.kaariainen@gmail.com>
2019-09-22 15:50:51 +0300

    osa3 notebackend progression

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let notes = [
   {
@@ -29,6 +32,49 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.json(notes)
 })
+
+app.get('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  response.json(note)
+})
+
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+
 
 const PORT = 9877
 app.listen(PORT, () => {
